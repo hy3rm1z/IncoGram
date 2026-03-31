0317d95a0e77264b654706c3c91880b0dbd4556e
hy3rm1z <snoserhr@gmail.com>
2026-03-31 13:54:18 +0400

    Create conf.lua for LÖVE framework configuration

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- LÖVE framework configuration for Incogram
+
+function love.conf(t)
+    t.window.title = "Incogram"
+    t.window.width = 800
+    t.window.height = 600
+    t.console = true
+    t.window.resizable = false
+    t.window.fullscreen = false
+end
