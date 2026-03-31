35840934c0408cbeb9b2a9c5fe69dcec324bd450
hy3rm1z <snoserhr@gmail.com>
2026-03-31 14:02:05 +0400

    Create navigation module for screen navigation system

---
+++ modules/navigation.lua
@@ -0,0 +1,17 @@
+-- Screen Navigation System Module
+
+local Navigation = {}
+
+-- Function to navigate to a screen
+function Navigation.goToScreen(screenName)
+    print("Navigating to " .. screenName)
+    -- Add code here to handle the actual navigation logic
+end
+
+-- Function to go back to the previous screen
+function Navigation.goBack()
+    print("Going back to the previous screen")
+    -- Add code here to handle going back
+end
+
+return Navigation
